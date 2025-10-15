799cf9f64b9a9bbb74adf7cad6ce67e2ca87dce5
Kareemmohameed <karemmohamed254452@gmail.com>
2025-10-15 21:58:35 +0300

    fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
-import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter basename="/Ecommerce">
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
+
+
+
